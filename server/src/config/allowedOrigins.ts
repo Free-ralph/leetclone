8e370f26231c7de43153fad12e3e2df44c864c2b
Free-Ralph <okosachideragia@gmail.com>
2024-01-30 01:21:12 +0100

    updated allowedOrigins

--- server/src/config/allowedOrigins.ts
+++ server/src/config/allowedOrigins.ts
@@ -1,8 +1,14 @@
-const allowedOrigins = [
-  "http://127.0.0.1:5500",
-  "http://localhost:3500",
-  "http://localhost:3000",
-  "http://localhost:5173",
-];
+let allowedOrigins : string[];
+
+if (process.env.PROD) {
+  allowedOrigins = ["leetclone-roan.vercel.app"];
+} else {
+  allowedOrigins = [
+    "http://127.0.0.1:5500",
+    "http://localhost:3500",
+    "http://localhost:3000",
+    "http://localhost:5173",
+  ];
+}
 
 export default allowedOrigins;
--- server/src/config/corsOptions.ts
+++ server/src/config/corsOptions.ts
@@ -2,7 +2,8 @@ import allowedOrigins from "./allowedOrigins.js";
 
 const corsOptions = {
   origin: (origin: any, callback: any) => {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+    // for prod, where going to check if there's an origin, so vices like postman "without origin won't access" so !origin 
+    if (allowedOrigins.indexOf(origin) !== -1 || (!origin && !process.env.PROD)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
