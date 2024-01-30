dc6d4a4f9393b76a2486e0cd4ff52d3969f16091
Free-Ralph <okosachideragia@gmail.com>
2024-01-30 11:11:29 +0100

    added

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3500/api/v1";
+const BASE_URL = "https://leetclone-server.vercel.app/api/v1";
 // const BASE_URL = "https://leetclone-kappa.vercel.app/api/v1";
 export default axios.create({
   baseURL: BASE_URL,
