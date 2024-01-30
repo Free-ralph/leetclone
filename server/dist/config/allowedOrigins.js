16fa402c7a4701c1ad1e33731b1609354a109b14
Free-Ralph <okosachideragia@gmail.com>
2024-01-30 11:06:25 +0100

    Tv screen

--- server/dist/config/allowedOrigins.js
+++ server/dist/config/allowedOrigins.js
@@ -1,14 +1,12 @@
-let allowedOrigins;
-if (process.env.PROD) {
-    allowedOrigins = ["https://leetclone-roan.vercel.app"];
-}
-else {
-    allowedOrigins = [
-        "http://127.0.0.1:5500",
-        "http://localhost:3500",
-        "http://localhost:3000",
-        "http://localhost:5173",
-    ];
-}
+// if (process.env.PROD) {
+const allowedOrigins = ["https://leetclone-roan.vercel.app", "https://leetclone-roan.vercel.app/", "leetclone-roan.vercel.app"];
+// } else {
+//   allowedOrigins = [
+//     "http://127.0.0.1:5500",
+//     "http://localhost:3500",
+//     "http://localhost:3000",
+//     "http://localhost:5173",
+//   ];
+// }
 export default allowedOrigins;
 //# sourceMappingURL=allowedOrigins.js.map
